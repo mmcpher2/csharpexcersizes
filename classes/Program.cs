4c09aa7509aa3a69a8673242dc0cc63afd8892a8
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-06 11:51:54 -0600

    Starting Classes es.

---
+++ classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    class CurrentEmployees
+    {
+
+    }
+    public class Company
+{
+    /*
+        Some readonly properties
+    */
+    public string Name { get; }
+    public DateTime CreatedOn { get; }
+
+    // Create a property for holding a list of current employees
+    public List<CurrentEmployees> currentEmployees { get; set; }
+    // Create a method that allows external code to add an employee
+
+    // Create a method that allows external code to remove an employee
+
+    /*
+        Create a constructor method that accepts two arguments:
+            1. The name of the company
+            2. The date it was created
+
+        The constructor will set the value of the public properties
+    */
+}
+}
