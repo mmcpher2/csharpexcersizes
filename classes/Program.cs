1ae966ceaf5c7fc1309cbf30d0e6d1ea46b2e21b
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-08 14:31:32 -0600

    so lost

--- classes/Program.cs
+++ classes/Program.cs
@@ -9,43 +9,40 @@ namespace classes {
                 public string Title {get; set; }
                 public DateTime CreatedOn {get; set; }
             }
-
-    public class Company
-        {
-            /*
-                Some readonly properties
-            */
-            public string Name { get; }
-            public DateTime CreatedOn { get; }
-            // Create a property for    holding a list of current employees
-            public List<Employee> currentEmployees { get; set; }
-            // Create a method that allows external code to add an employee
-            public void addEmployee (string Name, string Title, DateTime CreatedOn)
-                {
-                    currentEmployees.Add(this, Employee);
-                }
-            // Create a method that allows external code to remove an employee
-            public void removeEmployee (string Name, string Title, DateTime CreatedOn)
-                {
-                    currentEmployees.Remove(this, currentEmployee);
-                }
+        public class Company
+            {
                 /*
-                    Create a constructor method that accepts two arguments:
-                        1. The name of the company
-                        2. The date it was created
-
-                    The constructor will set the value of the public properties
+                    Some readonly properties
                 */
-            public Company (String Name, DateTime CreateOn) {
-                Name = "Ryans Company";
-                CreatedOn = new DateTime();
-                }
-        }
+                public string Name { get; }
+                public DateTime CreatedOn { get; }
+                // Create a property for    holding a list of current employees
+                public List<Employee> currentEmployees { get; set; }
+                // // Create a method that allows external code to add an employee
+                // public void addEmployee (string Name, string Title, DateTime CreatedOn)
+                //     {
+                //         currentEmployees.Add();
+                //     }
+                // // Create a method that allows external code to remove an employee
+                // public void removeEmployee (string Name, string Title, DateTime CreatedOn)
+                //     {
+                //         currentEmployees.Remove();
+                //     }
+                    /*
+                        Create a constructor method that accepts two arguments:
+                            1. The name of the company
+                            2. The date it was created
+
+                        The constructor will set the value of the public properties
+                    */
+                public Company (String Name, DateTime CreateOn) {
+                    Name = "Ryans Company";
+                    CreatedOn = new DateTime();
+                    }
+            }
 
     public static void Main(string[] args)
         {
-            Company ryansCompany = new Company();
-
             Employee Ryan = new Employee();
             Ryan.Name = "Ryan";
             Ryan.Title = "President";
