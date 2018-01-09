3e74dec90200b2f82037236cfed0818b60a634a0
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-09 16:03:01 -0600

    finished ordering operations

--- linq/Program.cs
+++ linq/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace linq
 {
@@ -21,13 +21,24 @@ namespace linq
             Console.WriteLine(fruit);
             }
 
-        // Which of the following numbers are multiples of 4 or 6
-        List<int> numbers = new List<int>()
-        {
-            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
-        };
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
 
-        List<int> fourSixMultiples = numbers.Where(t => t % 4 = 0);
+            var descend =
+                from name in names
+                orderby name descending
+                select name;
+
+            foreach (string word in descend) {
+                Console.WriteLine(word);
+            }
         }
     }
 }
