b3f3a5fee234e11da14cfbbc7d6175b57b20aedb
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-10 10:47:45 -0600

    Moving to Bangazon excersizes

--- linq/Program.cs
+++ linq/Program.cs
@@ -98,33 +98,33 @@ namespace linq
             }
 
             // Build a collection of customers who are millionaires
-                    List<Customer> customers = new List<Customer>() {
-                        new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
-                        new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
-                        new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
-                        new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
-                        new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
-                        new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
-                        new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
-                        new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
-                        new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
-                        new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
-                    };
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
 
-                    // list of millionaires
-                    List<Customer> millionaires = customers.Where(c => c.Balance >= 1000000).ToList();
+            // list of millionaires
+            List<Customer> millionaires = customers.Where(c => c.Balance >= 1000000).ToList();
 
-                    var groupsOfMillionaires =
-                        from customer in millionaires
-                        group customers by customer.Bank into milGroup
-                        select new {
-                            Bank = milGroup.Key,
-                            NumberOfMils = milGroup.Count()
-                        };
+            var groupsOfMillionaires =
+                from customer in millionaires
+                group customers by customer.Bank into milGroup
+                select new {
+                    Bank = milGroup.Key,
+                    NumberOfMils = milGroup.Count()
+                };
 
-                    foreach (var bank in groupsOfMillionaires){
-                        Console.WriteLine($"{bank.Bank}: {bank.NumberOfMils}");
-                }
+            foreach (var bank in groupsOfMillionaires){
+                Console.WriteLine($"{bank.Bank}: {bank.NumberOfMils}");
+            }
         }
         public class Customer
         {
@@ -132,6 +132,21 @@ namespace linq
             public double Balance { get; set; }
             public string Bank { get; set; }
         }
+
+                // Define a bank
+        public class Bank
+        {
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+        }
+
+        // Create some banks and store in a List
+        List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+        };
     }
 }
 
