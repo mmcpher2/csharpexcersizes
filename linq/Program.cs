2b8548b2a69d67871d2128d7ce7b9dabe5c9d6f3
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-10 09:01:21 -0600

    Banks populating

--- linq/Program.cs
+++ linq/Program.cs
@@ -97,7 +97,42 @@ namespace linq
             Console.WriteLine(Math.Sqrt(number));
             }
 
-            
+            // Build a collection of customers who are millionaires
+                    List<Customer> customers = new List<Customer>() {
+                        new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                        new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                        new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                        new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                        new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                        new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                        new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                        new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                        new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                        new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+                    };
+
+                    // list of millionaires
+                    List<Customer> millionaires = customers.Where(c => c.Balance >= 1000000).ToList();
+
+                    var groupsOfMillionaires =
+                        from customer in millionaires
+                        group customers by customer.Bank into milGroup
+                        select new {
+                            Bank = milGroup.Key,
+                            NumberOfMils = milGroup.Count()
+                        };
+
+                    foreach (var bank in groupsOfMillionaires){
+                        Console.WriteLine($"{bank.Bank}: {bank.NumberOfMils}");
+                }
+        }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
         }
     }
 }
+
+
