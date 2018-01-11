e7380662710f589e92a5dd54fb2f53ce16d7d4c0
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-11 10:56:05 -0600

    finished family ex

---
+++ family_dictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+                {"name", "Kathleen"},
+                {"age", "23"}
+            });
+            myFamily.Add("brother", new Dictionary<string, string>(){
+                {"name", "Cameron"},
+                {"age", "30"}
+            });
+            myFamily.Add("dad", new Dictionary<string, string>(){
+                {"name", "Mike"},
+                {"age", "59"}
+            });
+            myFamily.Add("mom", new Dictionary<string, string>(){
+                {"name", "Lisa"},
+                {"age", "57"}
+            });
+
+            foreach(KeyValuePair<string, Dictionary<string, string>> familyMember in myFamily) {
+                string familyMemberType = familyMember.Key;
+
+                var eachFamilyMember = familyMember.Value;
+                foreach(KeyValuePair<string, string> nameAndAge in eachFamilyMember){
+                    Console.WriteLine("");
+                    if (nameAndAge.Key == "name"){
+                        string personName = nameAndAge.Value;
+                        Console.Write($"{personName} is my {familyMemberType} and is ");
+                    } else {
+                        string personAge = nameAndAge.Value;
+                        Console.Write($"{personAge} years old.");
+                    }
+                }
+            }
+        }
+    }
+}
