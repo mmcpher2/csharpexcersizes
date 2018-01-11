36a797c45e022a933b847317af9ed69ba2898e94
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-11 15:44:55 -0600

    updating files

---
+++ bangazon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bangazon
+{
+    public class Department
+    {
+        public string _name;
+        public string _supervisor;
+        public int _employee_count;
+        public double Budget;
+
+        // Constructor method
+        public Department(string name, string supervisor, int employees)
+        {
+            _name = name;
+            _supervisor = supervisor;
+            _employee_count = employees;
+        }
+        public virtual void setBudget(double budget) => this.Budget = 50000;
+    }
+}
