5ead4911180e3d6ec29b428be00c55c35602d5fc
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-17 16:51:17 -0600

    species and walking tests work

--- animals/Animals.Tests/AnimalsShould.cs
+++ animals/Animals.Tests/AnimalsShould.cs
@@ -15,13 +15,25 @@ namespace Animals.Tests
 
 
         [Fact]
-
         public void nameAnimal()
         {
-            _animal.nameIt("Jack Rabbit");
-
+            _animal.nameOfAnimal("Jack Rabbit");
             Assert.Equal(_animal.Name, "Jack Rabbit");
+        }
+        [Fact]
+        public void speciesAnimal()
+        {
+            _animal.speciesOfAnimal("Silly Rabbit");
+            Assert.Equal(_animal.Species, "Silly Rabbit");
+        }
+        [Fact]
+        public void checkWalkingSpeed()
+        {
+            _animal.walk("fast");
+            _dog.walk("slow");
 
+            Assert.Equal(_animal.WalkingSpeed, "fast");
+            Assert.Equal(_dog.WalkingSpeed, "slow");
         }
     }
 }
