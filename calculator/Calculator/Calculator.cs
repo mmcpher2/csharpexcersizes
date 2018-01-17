529279cbdac2295a276cb84f39aff3315052a700
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-17 11:35:21 -0600

    not working

---
+++ calculator/Calculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(double firstNumber, double secondNumber)
+        {
+            double sum = firstNumber + secondNumber;
+            return sum;
+        }
+    }
+}
