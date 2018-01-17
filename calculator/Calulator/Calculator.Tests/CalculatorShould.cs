ca26741e1a472d62a0ab4158c43b0a585193e8cd
Ryan Mcpherson <mmcpher2@gmail.com>
2018-01-17 10:20:37 -0600

    starting Calculator Test

---
+++ calculator/Calulator/Calculator.Tests/CalculatorShould.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorShould
+    {
+        public CalculatorShould()
+        {
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
